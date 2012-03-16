3e8ae8cd11e41ce9e918b44b905b178544c27d42
Connie Guo <connie.guo107@gmail.com>
2012-03-15 17:23:43 -0700

    added spec tests for views

--- spec/views/admin/content/new_spec.rb
+++ spec/views/admin/content/new_spec.rb
@@ -34,4 +34,38 @@ describe "admin/content/new.html.erb" do
     assign(:resources, [])
     render
   end
+
+  before do
+    assign(:images, [])
+    assign(:macros, [])
+    assign(:resources, [])
+  end
+
+  it "renders a header tag titled Merge if user is an admin" do
+    render
+    rendered.should have_selector("h4", :content=>"Merge")
+  end
+
+  it "renders the merge articles partial if user is an admin" do
+    render
+    rendered.should render_template(:partial => "admin/content/_merge")
+  end
+
+  it "should render the merge form if user is an admin" do
+  end
+
+  it "should not render a header tag titled Merge if user is a non-admin" do
+    non_admin = stub_model(User, :settings => {:editor => 'simple'}, :admin? => false)
+    view.stub(:current_user) {non_admin}
+    render
+    rendered.should_not have_selector("h4", :content=>"Merge")
+  end
+
+  it "should not render the merge articles partial if user is a non-admin" do
+    non_admin = stub_model(User, :settings => {:editor => 'simple'}, :admin? => false)
+    view.stub(:current_user) {non_admin}
+    render
+    render.should_not render_template(:partial => "admin/content/_merge")
+  end
+
 end
